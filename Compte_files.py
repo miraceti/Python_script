f95ed992e603e8f03fed5acb42ce0deec0bb4e1b
Rico Miraceti <elecam@gmail.com>
2021-02-07 15:49:09 +0100

    local only

--- Compte_files.py
+++ Compte_files.py
@@ -1,5 +1,5 @@
 import fnmatch
 import os
-# pth = "//172.16.10.7/info$/PROGRAMMATION/VBScripts/SATEC/DIAC/SFTP_TOUS/DIAC_20210203"
+
 pth = "C:\Windows"
 print(len(fnmatch.filter(os.listdir(pth), "*")))
