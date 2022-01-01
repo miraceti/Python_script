82e9e31f6f07d96d117a15cd4a21bc93e93bcde9
Rico Miraceti <elecam@gmail.com>
2022-01-01 02:23:08 +0100

    v2

--- brocode2_tk.py
+++ brocode2_tk.py
@@ -5,14 +5,18 @@ window = Tk()
 def submit():
     username = entry.get()
     print("Hello "+ username)
+    entry.config()
     
 def delete():
     entry.delete(0, END)
     
 def backspace():
     entry.delete(len(entry.get())-1,END)
+    
 
-entry = Entry(window, font=("arial",50))
+
+entry = Entry(window, font=("arial",50), fg="#00ff00", bg="black")
+entry.insert(0, "test eponge")
 entry.pack(side=LEFT)
 
 
